925fd43702ea9b7bb589e8fb7812a2aab691946e
obaida <aljarraho18@gmail.com>
2022-02-17 01:44:21 +0100

    add products loop

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 function App() {
+  const [products, setProducts] = useState([
+    { name: "product1", price: 100.0 },
+    { name: "product2", price: 200.0 },
+  ]);
+  const addProduct = () =>
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        name: "product" + (prevState.length + 1),
+        price: prevState.length * 100 + 100,
+      },
+    ]);
   return (
     <div className="App">
-      <h1>welcome to ecomerce App</h1>
+      <h1 style={{ color: "orange", textAlign: "center" }}>
+        welcome to ecomerce App
+      </h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name}-{item.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
     </div>
   );
 }
